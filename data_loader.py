0efac903f78d44f3c0034db6a2b698c7f267bdb1
Vidak Kazic <vidakdk@gmail.com>
2020-07-11 11:14:32 +0200

    wip: dataloader changes

--- data_loader.py
+++ data_loader.py
@@ -14,46 +14,24 @@ from PIL import Image
 #==========================dataset load==========================
 class RescaleT(object):
 
-	def __init__(self,output_size):
-		assert isinstance(output_size,(int,tuple))
+	def __init__(self, output_size):
+		assert isinstance(output_size, (int, tuple))
 		self.output_size = output_size
 
-	def __call__(self,sample):
-		imidx, image, label = sample['imidx'], sample['image'],sample['label']
+	def __call__(self, image):
+		new_size = (self.output_size, self.output_size)
+		img = transform.resize(image, new_size, mode='constant')
+		return img
 
-		h, w = image.shape[:2]
-
-		if isinstance(self.output_size,int):
-			if h > w:
-				new_h, new_w = self.output_size*h/w,self.output_size
-			else:
-				new_h, new_w = self.output_size,self.output_size*w/h
-		else:
-			new_h, new_w = self.output_size
-
-		new_h, new_w = int(new_h), int(new_w)
-
-		# #resize the image to new_h x new_w and convert image from range [0,255] to [0,1]
-		# img = transform.resize(image,(new_h,new_w),mode='constant')
-		# lbl = transform.resize(label,(new_h,new_w),mode='constant', order=0, preserve_range=True)
-
-		img = transform.resize(image,(self.output_size,self.output_size),mode='constant')
-		lbl = transform.resize(label,(self.output_size,self.output_size),mode='constant', order=0, preserve_range=True)
-
-		return {'imidx':imidx, 'image':img,'label':lbl}
 
 class Rescale(object):
-
 	def __init__(self,output_size):
-		assert isinstance(output_size,(int,tuple))
+		assert isinstance(output_size, (int,tuple))
 		self.output_size = output_size
 
-	def __call__(self,sample):
-		imidx, image, label = sample['imidx'], sample['image'],sample['label']
-
+	def __call__(self, image):
 		if random.random() >= 0.5:
 			image = image[::-1]
-			label = label[::-1]
 
 		h, w = image.shape[:2]
 
@@ -68,10 +46,10 @@ class Rescale(object):
 		new_h, new_w = int(new_h), int(new_w)
 
 		# #resize the image to new_h x new_w and convert image from range [0,255] to [0,1]
-		img = transform.resize(image,(new_h,new_w),mode='constant')
-		lbl = transform.resize(label,(new_h,new_w),mode='constant', order=0, preserve_range=True)
+		img = transform.resize(image, (new_h,new_w), mode='constant')
 
-		return {'imidx':imidx, 'image':img,'label':lbl}
+		return img
+
 
 class RandomCrop(object):
 
@@ -82,12 +60,10 @@ class RandomCrop(object):
 		else:
 			assert len(output_size) == 2
 			self.output_size = output_size
-	def __call__(self,sample):
-		imidx, image, label = sample['imidx'], sample['image'], sample['label']
 
+	def __call__(self, image):
 		if random.random() >= 0.5:
 			image = image[::-1]
-			label = label[::-1]
 
 		h, w = image.shape[:2]
 		new_h, new_w = self.output_size
@@ -96,25 +72,17 @@ class RandomCrop(object):
 		left = np.random.randint(0, w - new_w)
 
 		image = image[top: top + new_h, left: left + new_w]
-		label = label[top: top + new_h, left: left + new_w]
 
-		return {'imidx':imidx,'image':image, 'label':label}
+		return image
+
 
 class ToTensor(object):
 	"""Convert ndarrays in sample to Tensors."""
 
-	def __call__(self, sample):
-
-		imidx, image, label = sample['imidx'], sample['image'], sample['label']
-
+	def __call__(self, image):
 		tmpImg = np.zeros((image.shape[0],image.shape[1],3))
-		tmpLbl = np.zeros(label.shape)
 
 		image = image/np.max(image)
-		if(np.max(label)<1e-6):
-			label = label
-		else:
-			label = label/np.max(label)
 
 		if image.shape[2]==1:
 			tmpImg[:,:,0] = (image[:,:,0]-0.485)/0.229
@@ -125,31 +93,18 @@ class ToTensor(object):
 			tmpImg[:,:,1] = (image[:,:,1]-0.456)/0.224
 			tmpImg[:,:,2] = (image[:,:,2]-0.406)/0.225
 
-		tmpLbl[:,:,0] = label[:,:,0]
-
 		# change the r,g,b to b,r,g from [0,255] to [0,1]
-		#transforms.Normalize(mean = (0.485, 0.456, 0.406), std = (0.229, 0.224, 0.225))
 		tmpImg = tmpImg.transpose((2, 0, 1))
-		tmpLbl = label.transpose((2, 0, 1))
+		image = torch.from_numpy(tmpImg)
+		return image
 
-		return {'imidx':torch.from_numpy(imidx), 'image': torch.from_numpy(tmpImg), 'label': torch.from_numpy(tmpLbl)}
 
 class ToTensorLab(object):
 	"""Convert ndarrays in sample to Tensors."""
 	def __init__(self,flag=0):
 		self.flag = flag
 
-	def __call__(self, sample):
-
-		imidx, image, label =sample['imidx'], sample['image'], sample['label']
-
-		tmpLbl = np.zeros(label.shape)
-
-		if(np.max(label)<1e-6):
-			label = label
-		else:
-			label = label/np.max(label)
-
+	def __call__(self, image):
 		# change the color space
 		if self.flag == 2: # with rgb and Lab colors
 			tmpImg = np.zeros((image.shape[0],image.shape[1],6))
@@ -213,14 +168,12 @@ class ToTensorLab(object):
 				tmpImg[:,:,1] = (image[:,:,1]-0.456)/0.224
 				tmpImg[:,:,2] = (image[:,:,2]-0.406)/0.225
 
-		tmpLbl[:,:,0] = label[:,:,0]
-
 		# change the r,g,b to b,r,g from [0,255] to [0,1]
 		#transforms.Normalize(mean = (0.485, 0.456, 0.406), std = (0.229, 0.224, 0.225))
 		tmpImg = tmpImg.transpose((2, 0, 1))
-		tmpLbl = label.transpose((2, 0, 1))
+		image = torch.from_numpy(tmpImg)
+		return image
 
-		return {'imidx':torch.from_numpy(imidx), 'image': torch.from_numpy(tmpImg), 'label': torch.from_numpy(tmpLbl)}
 
 class SalObjDataset(Dataset):
 	def __init__(self,img_name_list,lbl_name_list,transform=None):
